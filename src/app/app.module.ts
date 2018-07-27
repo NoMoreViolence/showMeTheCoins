139fb8a5c6443f3dd4ca51d2f3f624f3340630d3
NoMoreViolence <ljh86029926@gmail.com>
2018-07-27 12:27:53 +0900

    컴포넌트 정의

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoinsComponent } from './coins/coins.component';
+import { SelectedComponent } from './selected/selected.component';
+import { CoinModelComponent } from './coin-model/coin-model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoinsComponent,
+    SelectedComponent,
+    CoinModelComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/coin-model/coin-model.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coin-model',
+  templateUrl: './coin-model.component.html',
+  styleUrls: ['./coin-model.component.css']
+})
+export class CoinModelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
