d5198740c7481b13e9a79ed7db176d2fd2ea7bef
NoMoreViolence <ljh86029926@gmail.com>
2018-08-01 09:59:49 +0900

    error fix

--- src/functions/makeArray.ts
+++ src/functions/makeArray.ts
@@ -4,12 +4,11 @@ import objectCheck from './objectCheck';
 // [{}, {}, {}, {}, {}, {}, {}, {}] 형태의 배열 객체로 변환해주는 함수
 const makeArray = (bigObject: {}): Array<any> => {
   const list = [];
-
-  Object.keys(bigObject).map(key => {
-    if (objectCheck(key)) {
+  if (objectCheck(bigObject)) {
+    Object.keys(bigObject).map(key => {
       list.push(bigObject[key]);
-    }
-  });
+    });
+  }
 
   return list;
 };
