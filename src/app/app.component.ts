43d872f5b831b506f6e5d592a9cd03c4df9d3a2f
NoMoreViolence <ljh86029926@gmail.com>
2018-08-03 09:51:21 +0900

    root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Store } from './store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(private store: Store) {}
+
+  async ngOnInit() {
+    // First loading
+    this.store.loadCoinData('환영합니다 !');
+  }
 }
