280fca7aa851e605137d5d17e0054509392121bf
NoMoreViolence <ljh86029926@gmail.com>
2018-08-04 22:17:39 +0900

    add Routing, Toast module & store service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { routing } from './app.routing';
+
+import { Store } from './store.service';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 import { CoinsComponent } from './coins/coins.component';
 import { SelectedComponent } from './selected/selected.component';
 import { CoinModelComponent } from './coin-model/coin-model.component';
+import { MainComponent } from './main/main.component';
+import { UnderComponent } from './under/under.component';
+
+// Toast
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoinsComponent,
     SelectedComponent,
-    CoinModelComponent
+    CoinModelComponent,
+    SearchBarComponent,
+    MainComponent,
+    UnderComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, RouterModule, routing, HttpClientModule, BrowserAnimationsModule, ToastrModule.forRoot()],
+  providers: [Store],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {}
+}
