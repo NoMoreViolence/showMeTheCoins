d226eeaafa93ee1d4cb8b876234a503d4666eb86
NoMoreViolence <ljh86029926@gmail.com>
2018-08-08 12:10:54 +0900

    change component name & complete loading spinner

---
+++ src/app/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from 'src/app/store.service';
+
+@Component({
+  selector: 'app-loading-spinner',
+  templateUrl: './loading-spinner.component.html',
+  styleUrls: ['./loading-spinner.component.css']
+})
+export class LoadingSpinnerComponent implements OnInit {
+  constructor(private store: Store) {}
+
+  ngOnInit() {}
+}
