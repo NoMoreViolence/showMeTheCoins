0df88e9f535d9b068cbf57a2af87f5fc9dca29ee
NoMoreViolence <ljh86029926@gmail.com>
2018-08-09 15:55:25 +0900

    coins component mobile ui complete

--- src/app/coins/coins.component.ts
+++ src/app/coins/coins.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from 'src/app/store.service';
 
 @Component({
   selector: 'app-coins',
   templateUrl: './coins.component.html',
-  styleUrls: ['./coins.component.css']
+  styleUrls: ['./coins.component.scss']
 })
 export class CoinsComponent implements OnInit {
+  constructor(private store: Store) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
