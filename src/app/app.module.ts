6b39f4ab24be8961533031871a9f9c97ea14af80
NoMoreViolence <ljh86029926@gmail.com>
2018-08-13 16:07:16 +0900

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { routing } from './app.routing';
 
 import { Store } from './store.service';
+import { ChangeToColorPipe, ComaToNumberPipe } from './pipes';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { NavigateComponent } from './navigate/navigate.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 import { CoinsComponent } from './coins/coins.component';
 import { SelectedComponent } from './selected/selected.component';
 import { CoinModelComponent } from './coin-model/coin-model.component';
-import { MainComponent } from './main/main.component';
 import { UnderComponent } from './under/under.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 // Toast
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -21,13 +25,18 @@ import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
+    ChangeToColorPipe,
+    ComaToNumberPipe,
     AppComponent,
+    MainComponent,
+    SearchBarComponent,
+    NavigateComponent,
+    LoadingSpinnerComponent,
     CoinsComponent,
     SelectedComponent,
     CoinModelComponent,
-    SearchBarComponent,
-    MainComponent,
-    UnderComponent
+    UnderComponent,
+    NotFoundComponent
   ],
   imports: [BrowserModule, RouterModule, routing, HttpClientModule, BrowserAnimationsModule, ToastrModule.forRoot()],
   providers: [Store],
