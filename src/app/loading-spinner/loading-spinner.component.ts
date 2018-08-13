5be9819c32abad0233c82b5a43a14b98b744e9dd
NoMoreViolence <ljh86029926@gmail.com>
2018-08-13 16:08:02 +0900

    loading spinner component

--- src/app/loading-spinner/loading-spinner.component.ts
+++ src/app/loading-spinner/loading-spinner.component.ts
@@ -4,7 +4,7 @@ import { Store } from 'src/app/store.service';
 @Component({
   selector: 'app-loading-spinner',
   templateUrl: './loading-spinner.component.html',
-  styleUrls: ['./loading-spinner.component.css']
+  styleUrls: ['./loading-spinner.component.scss']
 })
 export class LoadingSpinnerComponent implements OnInit {
   constructor(private store: Store) {}
