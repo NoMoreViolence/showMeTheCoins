e5c6def50c41572ab0fc5db7b5d77278fc1485f7
NoMoreViolence <ljh86029926@gmail.com>
2018-08-17 16:54:23 +0900

    Delete rxjs expression - unnecessary

--- src/app/coins/coins.component.ts
+++ src/app/coins/coins.component.ts
@@ -1,8 +1,4 @@
 import { Component, AfterViewInit } from '@angular/core';
-
-import { from } from 'rxjs';
-import { map, find } from 'rxjs/operators';
-
 import { Store } from 'src/app/store.service';
 
 @Component({
@@ -13,20 +9,10 @@ import { Store } from 'src/app/store.service';
 export class CoinsComponent implements AfterViewInit {
   constructor(private store: Store) {}
 
-  selected = (value: [string, string | undefined, string | undefined]) => {
-    let returnData = '';
-
-    from(value)
-      .pipe(
-        map((obj: string, i: number) => obj === this.store.userChoice[i]),
-        find((obj: boolean) => obj === false)
-      )
-      .subscribe(data => {
-        returnData = data === undefined ? 'selected' : '';
-      });
-
-    return returnData;
-  };
+  selected = (value: [string, string | undefined, string | undefined]) =>
+    value.map((obj: string, i: number) => obj === this.store.userChoice[i]).find((obj: boolean) => obj === false) === undefined
+      ? 'selected'
+      : '';
 
   ngAfterViewInit = () => this.store.setScroll(this.store.scroll[this.store.urlNumber || 0]);
 }
