8a72570507411a40e1313317b4edb6006370d6da
NoMoreViolence <ljh86029926@gmail.com>
2018-08-17 16:12:01 +0900

    coins view change - not yet

--- src/app/coins/coins.component.ts
+++ src/app/coins/coins.component.ts
@@ -1,4 +1,8 @@
 import { Component, AfterViewInit } from '@angular/core';
+
+import { from } from 'rxjs';
+import { map, find } from 'rxjs/operators';
+
 import { Store } from 'src/app/store.service';
 
 @Component({
@@ -9,7 +13,20 @@ import { Store } from 'src/app/store.service';
 export class CoinsComponent implements AfterViewInit {
   constructor(private store: Store) {}
 
-  ngAfterViewInit() {
-    this.store.setScroll(this.store.scroll[this.store.urlNumber || 0]);
-  }
+  selected = (value: [string, string | undefined, string | undefined]) => {
+    let returnData = '';
+
+    from(value)
+      .pipe(
+        map((obj: string, i: number) => obj === this.store.userChoice[i]),
+        find((obj: boolean) => obj === false)
+      )
+      .subscribe(data => {
+        returnData = data === undefined ? 'selected' : '';
+      });
+
+    return returnData;
+  };
+
+  ngAfterViewInit = () => this.store.setScroll(this.store.scroll[this.store.urlNumber || 0]);
 }
