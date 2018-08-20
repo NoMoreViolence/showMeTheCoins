e1bd7e1bedcbf139b764097f9a522cf5ad2f0433
NoMoreViolence <ljh86029926@gmail.com>
2018-08-20 20:56:09 +0900

    Change interface structure

--- src/interfaces/SelectedCoin.ts
+++ src/interfaces/SelectedCoin.ts
@@ -1,27 +1,11 @@
 import SelectedCoinData from './SelectedCoinData';
 
 interface SelectedCoin {
-  KRW: {
-    Aggregated?: boolean;
-    Data: SelectedCoinData;
-    Message: string;
-    Response: string;
-    Type?: number;
-  };
-  USD: {
-    Aggregated?: boolean;
-    Data: SelectedCoinData;
-    Message: string;
-    Response: string;
-    Type?: number;
-  };
-  BTC: {
-    Aggregated?: boolean;
-    Data: SelectedCoinData;
-    Message: string;
-    Response: string;
-    Type?: number;
-  };
+  Aggregated?: boolean;
+  Data: SelectedCoinData;
+  Message: string;
+  Response: string;
+  Type?: number;
 }
 
 export default SelectedCoin;
