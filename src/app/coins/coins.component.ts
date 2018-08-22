18c1533d70044f51529fbe01962506c47b9568fd
NoMoreViolence <ljh86029926@gmail.com>
2018-08-22 18:53:02 +0900

    Separate more button & Set No data word

--- src/app/coins/coins.component.ts
+++ src/app/coins/coins.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { Store } from 'src/app/store.service';
 
 @Component({
@@ -6,15 +6,17 @@ import { Store } from 'src/app/store.service';
   templateUrl: './coins.component.html',
   styleUrls: ['./coins.component.scss']
 })
-export class CoinsComponent implements AfterViewInit {
-  constructor(private store: Store) {
-    this.store.loadAllCoinData('환영합니다 !');
-  }
+export class CoinsComponent implements OnInit, AfterViewInit {
+  constructor(private store: Store) {}
 
   selected = (value: [string, string | undefined, string | undefined]) =>
     value.map((obj: string, i: number) => obj === this.store.userChoice[i]).find((obj: boolean) => obj === false) === undefined
       ? 'selected'
       : '';
+
+  ngOnInit() {
+    this.store.loaded === 1 ? this.store.loadAllCoinData('환영합니다 !') : console.log('main page loaded');
+  }
 
   ngAfterViewInit() {
     this.store.setScroll(this.store.scroll[this.store.urlNumber || 0]);
