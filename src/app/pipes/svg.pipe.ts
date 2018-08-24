485097a594640dcebdda2b67adde7386e0241f61
NoMoreViolence <ljh86029926@gmail.com>
2018-08-24 22:52:16 +0900

    xhr to httpClient

--- src/app/pipes/svg.pipe.ts
+++ src/app/pipes/svg.pipe.ts
@@ -31,10 +31,10 @@
 //   }
 // }
 
-import { Pipe, PipeTransform, Sanitizer, SecurityContext } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 /*
  * Raise the value exponentially
  * Takes an exponent argument that defaults to 1.
@@ -44,18 +44,18 @@ import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/htt
  *   {{ 2 | exponentialStrength:10 }}
  *   formats to: 1024
 */
+
 @Pipe({ name: 'svgPipe' })
 export class SvgPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer, private http: HttpClient) {}
   transform(url: string): SafeUrl {
     return Observable.create(observer => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('get', url, true);
-      xhr.onloadend = () => {
-        const Data = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/svg+xml;utf8,' + xhr.responseText);
-        observer.next(Data);
-      };
-      xhr.send();
+      this.http.get(url).subscribe(
+        (data: string) => {},
+        (err: HttpErrorResponse) => {
+          observer.next(this.sanitizer.bypassSecurityTrustResourceUrl('data:image/svg+xml;utf8,' + err.error.text));
+        }
+      );
     });
   }
 }
